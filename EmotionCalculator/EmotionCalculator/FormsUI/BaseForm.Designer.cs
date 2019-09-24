416f7d334c7f82c8abf14e4cb7f10914e6458a6c
justaspaz <justas.pazarauskas@gmail.com>
2019-09-24 11:18:22 +0300

    WebCam sukurimas ant formos

--- EmotionCalculator/EmotionCalculator/FormsUI/BaseForm.Designer.cs
+++ EmotionCalculator/EmotionCalculator/FormsUI/BaseForm.Designer.cs
@@ -39,6 +39,13 @@
             this.operationResultLabel = new System.Windows.Forms.Label();
             this.errorTextLabel = new System.Windows.Forms.Label();
             this.errorLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // apiEndpointTextBox
@@ -136,11 +143,68 @@
             this.errorLabel.TabIndex = 9;
             this.errorLabel.Text = "Error:";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 387);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 37);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Turn ON WebCam";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(250, 388);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 36);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Take photo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(482, 387);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 36);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(653, 245);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(112, 21);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(237, 124);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(404, 227);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
             // BaseForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(769, 454);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.errorTextLabel);
             this.Controls.Add(this.errorLabel);
             this.Controls.Add(this.operationResultLabel);
@@ -155,6 +219,7 @@
             this.Name = "BaseForm";
             this.Text = "BaseForm";
             this.Load += new System.EventHandler(this.BaseForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -173,5 +238,11 @@
         private System.Windows.Forms.Label operationResultLabel;
         private System.Windows.Forms.Label errorTextLabel;
         private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
--- EmotionCalculator/EmotionCalculator/FormsUI/BaseForm.cs
+++ EmotionCalculator/EmotionCalculator/FormsUI/BaseForm.cs
@@ -3,6 +3,10 @@ using System;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static EmotionCalculator.EmotionCalculator.Tools.Web.ImageDownloader;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Drawing;
+
 
 namespace EmotionCalculator.EmotionCalculator.FormsUI
 {
@@ -15,7 +19,7 @@ namespace EmotionCalculator.EmotionCalculator.FormsUI
 
         private void BaseForm_Load(object sender, EventArgs e)
         {
-
+            LoadWebCam();
         }
 
         private void Label1_Click(object sender, EventArgs e)
@@ -36,6 +40,47 @@ namespace EmotionCalculator.EmotionCalculator.FormsUI
             string response = Task.Run(async () => await faceAPIRequester.RequestImageDataAsync(url)).Result;
 
             operationResultLabel.Text = response;
+        }
+        private FilterInfoCollection webcam;
+        private VideoCaptureDevice cam;
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            cam = new VideoCaptureDevice(webcam[comboBox1.SelectedIndex].MonikerString);
+            cam.NewFrame += new NewFrameEventHandler(cam_NewFrame);
+            cam.Start();
+
+        }
+        void cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bit = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = bit;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = @"D:\";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (cam.IsRunning)
+            {
+                cam.Stop();
+            }
+        }
+        private void LoadWebCam()
+        {
+            webcam = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo VideoCaptureDevice in webcam)
+            {
+                comboBox1.Items.Add(VideoCaptureDevice.Name);
+            }
+            comboBox1.SelectedIndex = 0;
         }
     }
 }
