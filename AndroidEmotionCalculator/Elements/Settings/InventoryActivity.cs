3b9e0bc0c556c8f18abd9e8e6275655473c8bb78
Mantas -Mantas <ggwpkmjm@outlook.com>
2019-11-27 21:49:01 +0200

    3

---
+++ AndroidEmotionCalculator/Elements/Settings/InventoryActivity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Support.V4.App;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AndroidEmotionCalculator.Fragments.Settings
+{
+    [Activity(Label = "InventoryActivity")]
+    public class InventoryActivity : Activity
+    {
+
+        List<InventoryModel> names;
+        ListView ListInventory;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Inventory);
+
+            names = new List<InventoryModel>();
+            names.Add(new InventoryModel() { Name = "Breeee", Image = null, Count = "1" });
+            names.Add(new InventoryModel() { Name = "Defoult", Image = null, Count = "2" });
+            names.Add(new InventoryModel() { Name = "Helouvyn", Image = null, Count = "3" });
+
+
+            ListInventory = (ListView)FindViewById(Resource.Id.listInventory);
+
+            InventoryAdaptor adaptor = new InventoryAdaptor(this, names);
+
+
+
+            ListInventory.Adapter = adaptor;
+            ListInventory.ItemClick += ListInventory_ItemClick;
+
+        }
+
+        private void ListInventory_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            StartNewActivity();
+            //Console.WriteLine(names[e.Position]);
+        }
+
+        void StartNewActivity()
+        {
+            Intent intent = new Intent(this, typeof(Settings.InventoryItemActivity));
+            StartActivity(intent);
+        }
+
+
+
+    }
+}
---
+++ AndroidEmotionCalculator/Elements/Settings/InventoryAdaptor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AndroidEmotionCalculator.Fragments.Settings
+{
+    class InventoryAdaptor : BaseAdapter<InventoryModel>
+    {
+
+        private List<InventoryModel> itemInfo;
+        private Context context;
+
+
+        public InventoryAdaptor(Context context, List<InventoryModel> itemInfo)
+        {
+            this.context = context;
+            this.itemInfo = itemInfo;
+        }
+
+        public override InventoryModel this[int position]
+        {
+            get { return itemInfo[position]; }
+        }
+        //=> throw new NotImplementedException();
+
+        public override int Count
+        {
+            get { return itemInfo.Count; }
+        }
+        //=> throw new NotImplementedException();
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+        //throw new NotImplementedException();
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View row = convertView;
+
+            if (row == null)
+            {
+                row = LayoutInflater.From(context).Inflate(Resource.Layout.inventoryItem, null, false);
+            }
+
+            TextView txtItem = row.FindViewById<TextView>(Resource.Id.txtInventoryItem);
+            txtItem.Text = itemInfo[position].Name;
+
+            return row;
+
+        }
+        //throw new NotImplementedException();
+
+
+
+
+
+    }
+}
