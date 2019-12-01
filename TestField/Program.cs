e23e3badbde8b10d2206bb8dc5aa927d503ea3d7
juliussyvis <55212122+juliussyvis@users.noreply.github.com>
2019-12-02 00:35:18 +0200

    !!!SETUP ANT localhost:5001!!! :)

--- TestField/Program.cs
+++ TestField/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace TestField
+﻿using System.Net.Http;
+
+namespace TestField
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        private static readonly string url = "https://localhost:5001/api/values";
 
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+            using (HttpResponseMessage response = await client.GetAsync(url))
+            {
+                System.Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+
+            System.Console.ReadLine();
         }
     }
 }
